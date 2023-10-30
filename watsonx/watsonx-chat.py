34e2ce61d794ac81bad496ec0544ef53028df0d7
Guangya Liu <gyliu@ibm.com>
2023-10-30 14:11:56 -0400

    watsonx update

--- watsonx/watsonx-chat.py
+++ watsonx/watsonx-chat.py
@@ -16,8 +16,6 @@ api_endpoint = os.getenv("GENAI_API", None)
 
 print("\n------------- Example (Model QA)-------------\n")
 
-max_cycles = 20
-
 bob_params = GenerateParams(
     decoding_method="sample",
     max_new_tokens=25,
@@ -28,35 +26,14 @@ bob_params = GenerateParams(
     top_p=1,
 )
 
-alice_params = GenerateParams(
-    decoding_method="sample",
-    max_new_tokens=45,
-    min_new_tokens=1,
-    stream=False,
-    temperature=0.05,
-    top_k=50,
-    top_p=1,
-)
-
 creds = Credentials(api_key, api_endpoint)
 bob_model = Model("google/flan-ul2", params=bob_params, credentials=creds)
-alice_model = Model("google/flan-t5-xxl", params=alice_params, credentials=creds)
 
 alice_q = "What is 1 + 1?"
 print(f"[Alice][Q] {alice_q}")
 
-for x in range(max_cycles):
-    bob_response = bob_model.generate([alice_q])
-    bob_a = bob_response[0].generated_text
-    print(f"[Bob][A] {bob_a}")
 
-    bob_q = "What is " + bob_a + " + " + bob_a + "?"
-    print(f"[Bob][Q] {bob_q}")
-
-    alice_response = alice_model.generate([bob_q])
-    alice_a = alice_response[0].generated_text
-    print(f"[Alice][A] {alice_a}")
-
-    alice_q = "What is " + alice_a + " + " + alice_a + "?"
-    print(f"[Alice][Q] {alice_q}")
-    time.sleep(0.5)
+aliceq = [alice_q]
+bob_response = bob_model.generate(aliceq)
+bob_a = bob_response[0].generated_text
+print(f"[Bob][A] {bob_a}")
