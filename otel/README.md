26658d16dbf5223e1a5697b93d40b580f9e45879
Guangya Liu <gyliu@ibm.com>
2023-11-09 13:54:20 -0500

    openai

---
+++ otel/README.md
@@ -0,0 +1,24 @@
+## Run OTEL Collector
+
+- goto https://github.com/open-telemetry/opentelemetry-collector-contrib/tree/main/examples/demo and run the collector via `docker compose up -d`
+
+## Install OTEL Packages
+
+```
+pip install opentelemetry-distro
+opentelemetry-bootstrap -a install
+pip install opentelemetry-exporter-otlp
+```
+
+## Run the openai script
+
+```
+python openai-otel.py
+```
+
+## Check from Jaeger
+
+Open http://0.0.0.0:16686/
+
+![](images/overall-jaeger.png)
+![](images/openai-jaeger.png)
---
+++ otel/openai-otel.py
@@ -0,0 +1,49 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+from opentelemetry import trace
+from opentelemetry.instrumentation.wsgi import collect_request_attributes
+from opentelemetry.propagate import extract
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+
+from opentelemetry.sdk.trace.export import (
+    BatchSpanProcessor,
+    ConsoleSpanExporter,
+)
+from opentelemetry.trace import (
+    SpanKind,
+    get_tracer_provider,
+    set_tracer_provider,
+)
+
+tracer_provider = TracerProvider(
+    resource=Resource.create({'service.name': 'my-service-openai-2'}),
+)
+
+# Create an OTLP Span Exporter
+otlp_exporter = OTLPSpanExporter(
+    endpoint="0.0.0.0:4317",  # Replace with your OTLP endpoint URL
+)
+
+# Add the exporter to the TracerProvider
+# tracer_provider.add_span_processor(BatchSpanProcessor(ConsoleSpanExporter()))  # Add any span processors you need
+tracer_provider.add_span_processor(BatchSpanProcessor(otlp_exporter))
+
+# Register the TracerProvider
+trace.set_tracer_provider(tracer_provider)
+
+tracer = trace.get_tracer_provider().get_tracer(__name__)
+
+with tracer.start_as_current_span("OpenAI_API_Call"):
+    # Your OpenAI API code here
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt="Generate some text...",
+        max_tokens=50,
+    )
