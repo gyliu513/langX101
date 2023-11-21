76c220f5f260227319647a124376ef700eed7d54
Guangya Liu <gyliu@ibm.com>
2023-11-20 22:37:29 -0500

    setup

---
+++ mylib/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mylib',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Any dependencies
+    ],
+)
---
+++ otel/opentelemetry-instrumentation-watsonx/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='opentelemetry',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Dependencies (if any)
+    ],
+)
