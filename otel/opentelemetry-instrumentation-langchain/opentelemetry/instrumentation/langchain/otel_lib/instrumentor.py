92a3fa6502dd26830bc0873e4b355e24aaf6c2c3
huang-cn <58250676+huang-cn@users.noreply.github.com>
2023-12-01 10:13:13 +0800

    langchain hdmanager instrument test (#116)

---
+++ otel/opentelemetry-instrumentation-langchain/opentelemetry/instrumentation/langchain/otel_lib/instrumentor.py
@@ -0,0 +1,41 @@
+from typing import Any, Optional
+
+from .tracer import OpenInferenceTracer
+from opentelemetry.trace import get_tracer
+from opentelemetry.instrumentation.langchain.version import __version__
+
+
+class LangChainHandlerInstrumentor:
+    """
+    Instruments the OpenInferenceTracer for LangChain automatically by patching the
+    BaseCallbackManager in LangChain.
+    """
+
+    def __init__(self, handeler: Optional[OpenInferenceTracer] = None) -> None:
+        self._handeler = handeler if handeler is not None else OpenInferenceTracer()
+
+    def instrument(self, *args, **kwargs) -> None:
+        try:
+            from langchain.callbacks.base import BaseCallbackManager
+        except ImportError:
+            # Raise a cleaner error if LangChain is not installed
+            raise ImportError(
+                "LangChain is not installed. Please install LangChain first to use the instrumentor"
+            )
+        tracer_provider = kwargs.get("tracer_provider", None)
+        tracer = get_tracer(__name__, __version__, tracer_provider)
+        self._handeler.tracer = tracer
+        source_init = BaseCallbackManager.__init__
+
+        # Keep track of the source init so we can tell if the patching occurred
+        self._source_callback_manager_init = source_init
+
+        handeler = self._handeler
+
+        # Patch the init method of the BaseCallbackManager to add the tracer
+        # to all callback managers
+        def patched_init(self: BaseCallbackManager, *args: Any, **kwargs: Any) -> None:
+            source_init(self, *args, **kwargs)
+            self.add_handler(handeler, True)
+
+        BaseCallbackManager.__init__ = patched_init
