1a7dc65e2c1fb93e1907e141fb3a04067658de74
Guangya Liu <gyliu@ibm.com>
2024-02-19 20:46:49 -0500

    lm3

--- langsmith/test_openai.py
+++ langsmith/test_openai.py
@@ -6,7 +6,18 @@ from langsmith import wrappers
 
 client = wrappers.wrap_openai(OpenAI())
 
-client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": "Say this is a test"}],
-)
+# client.chat.completions.create(
+#     model="gpt-3.5-turbo",
+#     messages=[{"role": "user", "content": "Say this is a test"}],
+# )
+
+from langsmith import traceable
+
+@traceable
+def my_function(text: str):
+    return client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": f"Say {text}"}],
+    )
+
+my_function("hello world")
