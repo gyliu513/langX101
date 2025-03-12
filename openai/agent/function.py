c24df368ffd29baae716e6a3290543d4307e9610
Guangya Liu <gyliu@ibm.com>
2025-03-12 14:27:10 -0400

    intial

---
+++ openai/agent/function.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import asyncio
+
+from agents import Agent, Runner, function_tool
+
+
+@function_tool
+def get_weather(city: str) -> str:
+    return f"The weather in {city} is sunny."
+
+
+agent = Agent(
+    name="Hello world",
+    instructions="You are a helpful agent.",
+    tools=[get_weather],
+)
+
+
+async def main():
+    result = await Runner.run(agent, input="What's the weather in Tokyo?")
+    print(result.final_output)
+    # The weather in Tokyo is sunny.
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
