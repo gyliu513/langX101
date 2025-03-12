8f1b1756cf81c25e5a69f67b1c4861065de07fe5
Guangya Liu <gyliu@ibm.com>
2025-03-12 14:27:36 -0400

    intial (#247)

---
+++ openai/agent/handoff.py
@@ -0,0 +1,31 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from agents import Agent, Runner
+import asyncio
+
+spanish_agent = Agent(
+    name="Spanish agent",
+    instructions="You only speak Spanish.",
+)
+
+english_agent = Agent(
+    name="English agent",
+    instructions="You only speak English",
+)
+
+triage_agent = Agent(
+    name="Triage agent",
+    instructions="Handoff to the appropriate agent based on the language of the request.",
+    handoffs=[spanish_agent, english_agent],
+)
+
+
+async def main():
+    result = await Runner.run(triage_agent, input="Hola, ¿cómo estás?")
+    print(result.final_output)
+    # ¡Hola! Estoy bien, gracias por preguntar. ¿Y tú, cómo estás?
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
