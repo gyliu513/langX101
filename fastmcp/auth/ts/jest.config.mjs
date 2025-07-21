b5a8c0c7625329a06529f15c8fb36c08485cfec2
Guangya Liu <gyliu@ibm.com>
2025-07-21 17:31:41 -0400

    Test (#306)
    * fix lint
    
    * test

---
+++ fastmcp/auth/ts/jest.config.mjs
@@ -0,0 +1,23 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      useESM: true,
+      tsconfig: 'tsconfig.test.json',
+    }],
+  },
+  collectCoverageFrom: [
+    '*.ts',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/dist/**',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testTimeout: 10000,
+  extensionsToTreatAsEsm: ['.ts'],
+}; 
