23b1d268076663a5b34662f9d0104bc4b5984b47
Chuan Ran <chuanran@Chuans-MacBook-Pro.local>
2025-11-01 21:58:38 -0400

    add support for multiple tools scenario in both server side and client side

--- fastmcp/streamable/client.py
+++ fastmcp/streamable/client.py
@@ -4,17 +4,19 @@ import asyncio
 
 transport = StreamableHttpTransport(url="http://0.0.0.0:8000/mcp")
 client = Client(transport)
-
 async def main():
     # Connection is established here
     async with client:
         print(f"Client connected: {client.is_connected()}")
         tools = await client.list_tools()
         print(f"Available tools: {tools}")
-        if any(tool.name == "greet" for tool in tools):
-            result = await client.call_tool("greet", {"name": "FastMCP 2.0"})
-            print(f"Greet result: {result}")
-            
+        for tool in tools:
+            if tool.name == "greet":
+                result = await client.call_tool("greet", {"name": "FastMCP 2.0"})
+                print(f"Greet result: {result}")
+            elif tool.name == "add":
+                result = await client.call_tool("add", {"a": 5, "b": 7})
+                print(f"Add result: {result}")
     # Connection is closed automatically here
     print(f"Client connected: {client.is_connected()}")
 
--- fastmcp/streamable/server.py
+++ fastmcp/streamable/server.py
@@ -1,8 +1,15 @@
 from fastmcp import FastMCP
-
 mcp = FastMCP(name="My MCP Server")
+
+@mcp.tool
+def add(a: int, b: int) -> int:
+    """Returns the sum of two integers."""
+    return a + b
 
 @mcp.tool
 def greet(name: str) -> str:
     """Returns a friendly greeting."""
     return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    mcp.run(transport="streamable-http", host="0.0.0.0", port=8000, path="/mcp")
